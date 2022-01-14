aa10f6f5034ab4044ee717a1a34439a38909ed0c
MeLea Hetrick <Hetrick.Melea@mayo.edu>
2022-01-14 12:19:09 -0600

    Mod1

--- PyPoll_Challenge_Mod1.py
+++ PyPoll_Challenge_Mod1.py
@@ -6,7 +6,7 @@ import csv
 from itertools import count
 import os
 from platform import java_ver
-
+from collections import Counter
 # Add a variable to load a file from a path.
 #---------------------------------------
 # I had to delete ".." ---> what is this? why did I have to delete it?
@@ -19,33 +19,42 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 total_votes = 0
 
 # Candidate Options and candidate votes.
-candidate_options = []
+candidate_options = ['Charles Casper Stockham', 'Diana DeGette', 'Raymon Anthony Doane']
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
 #create county list -> uses []
-county_list = []
+county_list = ['Jefferson', 'Denver', 'Arapahoe']
 #create county votes dictionary -> {}
 county_votes = {}
 
+print(county_votes)
 #create a dictionary for number of votes per cand/county
-Cand_votes_by_county = {}
+# Cand_votes_by_county = {
+#     'Charles Casper Stockham': ['Jefferson', 'Denver', 'Arapahoe'],
+#     'Diana DeGette': ['Jefferson', 'Denver', 'Arapahoe'],
+#     'Raymon Anthony Doane': ['Jefferson', 'Denver', 'Arapahoe']
+# }
 
-JeffersonVotes = 0
-Jefferson_by_candidate = {}
-# ArapahoeVotes = 0
-# Arapahoe_by_candidate = {}
-# DenverVotes = 0
-# denver_by_candidate = {}
-# Diana_Jefferson = 0
-# Diana_Arapahoe = 0
-# Diana_Denver = 0
-# Charles_Jefferson = 0
-# Charles_Arapahoe = 0
-# Charles_Denver = 0
-# Ray_Jefferson = 0
-# Ray_Arapahoe = 0
-# Ray_Denver = 0
+
+
+# print(Cand_votes_by_county)
+
+
+County1_by_candidate = {}
+
+County2_by_candidate = {}
+DenverVotes = 0
+denver_by_candidate = {}
+Diana_Jefferson = 0
+Diana_Arapahoe = 0
+Diana_Denver = 0
+Charles_Jefferson = 0
+Charles_Arapahoe = 0
+Charles_Denver = 0
+Ray_Jefferson = 0
+Ray_Arapahoe = 0
+Ray_Denver = 0
 
 # Read the csv and convert it into a list of dictionaries
 
@@ -65,27 +74,183 @@ with open(file_to_load) as election_data:
 
         # 3: Extract the county name from each row.
         county_name = row[1]
-        # 4a: Write an if statement that checks that the
-        # county does not match any existing county in the county list.
-        if county_name not in county_list:
+        # If the candidate does not match any existing candidate add it to
+        # the candidate list
+        if candidate_name == "Diana DeGette": 
+            if county_name == "Jefferson":
+                Diana_Jefferson += 1
+            elif county_name == "Arapahoe":
+                Diana_Arapahoe += 1
+            elif county_name == "Denver":
+                Diana_Denver += 1
+        if candidate_name == "Charles Casper Stockham": 
+            if county_name == "Jefferson":
+                Charles_Jefferson += 1
+            elif county_name == "Arapahoe":
+                Charles_Arapahoe += 1
+            elif county_name == "Denver":
+                Charles_Denver += 1
+        if candidate_name == "Raymon Anthony Doane": 
+            if county_name == "Jefferson":
+                Ray_Jefferson =+ 1
+            elif county_name == "Arapahoe":
+                Ray_Arapahoe += 1
+            elif county_name == "Denver":
+                Charles_Denver += 1
 
-            # 4b: Add the existing county to the list of counties.
-            county_list.append(county_name)
 
-         # 5: Add a vote to that county's vote count.
-        # county_votes[county_name] += 1
 
-        if candidate_name not in candidate_options:
+Diana_results = (
+    f"Diana DeGette:\n"
+    f"\tJefferson County: {Diana_Jefferson:,} votes \n"
+    f"\tArapahoe County: {Diana_Arapahoe:,} votes \n "
+    f"\tDenver County: {Diana_Denver:,} votes\n")
+Charles_results = (
+    f"Charles Casper Stockham:\n"
+    f"\tJefferson County: {Charles_Jefferson:,} votes \n"
+    f"\tArapahoe County: {Charles_Arapahoe:,} votes \n "
+    f"\tDenver County: {Charles_Denver:,} votes\n")
+Ray_results = (
+    f"Raymon Anthony Doane:\n"
+    f"\tJefferson County: {Ray_Jefferson:,} votes \n"
+    f"\tArapahoe County: {Ray_Arapahoe:,} votes \n "
+    f"\tDenver County: {Ray_Denver:,} votes\n")
+
+print("Each Candidate's Results by County\n----------------------------------")
+print(Diana_results)
+print(Charles_results)
+print(Ray_results)
+
+
+        # if county_name == 'Arapahoe':
+        #     County1_by_candidate[candidate_name] = 0
+        # County1_by_candidate[candidate_name]+=1
+
+
+        # elif county_name == "Arapahoe" :
+        #     County2_by_candidate[candidate_name] = 0
+        # County2_by_candidate[candidate_name] += 1
+
+
+# print(County1_by_candidate)
+# print(County2_by_candidate)
 
             # Add the candidate name to the candidate list.
-            candidate_options.append(candidate_name)
 
+            # And begin tracking that candidate's voter count.
+        #     candidate_votes[candidate_name] = 0
+
+        # # Add a vote to that candidate's count
+        # candidate_votes[candidate_name] += 1
+
+        # 4a: Write an if statement that checks that the
+        # county does not match any existing county in the county list.
+        # if county_name not in county_list:
+
+        #     # 4b: Add the existing county to the list of counties.
+        #     county_list.append(county_name)
+
+            # 4c: Begin tracking the county's vote count.
+        #     county_votes[county_name] = 0
+
+        # # 5: Add a vote to that county's vote count.
+        # county_votes[county_name] += 1
+
+        # if candidate_name in candidate_options:
+
+        #     # Add the candidate name to the candidate list.
+        #     # candidate_options.append(candidate_name)
+
+        #     # And begin tracking that candidate's voter count.
+        #     candidate_votes[candidate_name] = 0
+
+        # # Add a vote to that candidate's count
+        # candidate_votes[candidate_name] += 1
+
+        # # 4a: Write an if statement that checks that the
+        # # county does not match any existing county in the county list.
+        # if county_name not in county_list:
+
+        #     # 4b: Add the existing county to the list of counties.
+        #     county_list.append(county_name)
+
+        #     # 4c: Begin tracking the county's vote count.
+        #     county_votes[county_name] = 0
+
+        # # 5: Add a vote to that county's vote count.
+        # county_votes[county_name] += 1
+
+# print(candidate_votes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # If the candidate does not match any existing candidate add it to
+        # the candidate list
+                    # Add the candidate name to the candidate list.
+            # candidate_options.append(candidate_name)
+
+        # res = {
+        #     candidate: Counter(county) for candidate,county in Cand_votes_by_county.items()
+        # }
+    #     for candidate in Cand_votes_by_county.items():
+    #         res[candidate[0]] = {}
+    #         for county in candidate[1]:
+    #             res[candidate[0]][county] = 0
+    # res[candidate[0]][county]
+
+# print(res)
+
+        # Add a vote to that candidate's count
+
+            # begin tracking that candidate's voter count.
+            # Cand_votes_by_county[candidate_name][county_name] = 0
+
+        # Add a vote to that candidate's count
+        # Cand_votes_by_county[candidate_name][county_name] += 1
+    
+        # 4a: Write an if statement that checks that the
+        # county does not match any existing county in the county list.
+#         if county_name not in county_list:
+
+#             # 4b: Add the existing county to the list of counties.
+#             county_list.append(county_name)
+
+#             # 4c: Begin tracking the county's vote count.
+#             county_votes[county_name] = 0
+
+#         # 5: Add a vote to that county's vote count.
+#         county_votes[county_name] += 1
+
+#  #----------------------------------- I AM MISSING SOMETHING WHY DOESNT THIS WORK           
+#     for row in reader:
+#         for candidate_name in candidate_options:
+#             candidate_votes[candidate_name] = 0
+#             candidate_votes[candidate_name] =+ 1
+
+# for candidate_name in candidate_options:
+#     for county_name in county_list:
+#         Cand_votes_by_county[candidate_name]= 0
+#         print(candidate_name,county_name)
+# print(Cand_votes_by_county)
 # JeffersonVotes = [] is a list with the number of votes - maybe I don't need this
 # Jefferson_by_candidate = {} - a dictionary that will store the number of votes 
-    for candidate_name in candidate_options:
-        if county_name == "Jefferson":
-            Jefferson_by_candidate[candidate_name] = JeffersonVotes
-            JeffersonVotes =+ 1
+    # for candidate_name in candidate_options:
+    #     if county_name == "Jefferson":
+    #         Jefferson_by_candidate[candidate_name] = JeffersonVotes
+    #         JeffersonVotes =+ 1
             
 
 
@@ -130,36 +295,7 @@ with open(file_to_load) as election_data:
 
 # print(candidate_options)
 
-#         if candidate_name == "Diana DeGette": 
-#             if county_name == "Jefferson":
-#                 Diana_Jefferson += 1
-#             elif county_name == "Arapahoe":
-#                 Diana_Arapahoe += 1
-#             elif county_name == "Denver":
-#                 Diana_Denver += 1
-# #         if candidate_name == "Charles Casper Stockham": 
-# #             if county_name == "Jefferson":
-# #                 Charles_Jefferson += 1
-# #             elif county_name == "Arapahoe":
-# #                 Charles_Arapahoe += 1
-# #             elif county_name == "Denver":
-# #                 Charles_Denver += 1
+
 # # Ray_Jefferson = 0
 # # Ray_Arapahoe = 0
 # # Ray_Denver = 0
-
-
-# Diana_results = (
-#     f"Diana DeGette:\n"
-#     f"\tJefferson County: {Diana_Jefferson:,} votes \n"
-#     f"\tArapahoe County: {Diana_Arapahoe:,} votes \n "
-#     f"\tDenver County: {Diana_Denver:,} votes\n")
-# # Charles_results = (
-# #     f"Charles Casper Stockham:\n"
-# #     f"\tJefferson County: {Charles_Jefferson:,} votes \n"
-# #     f"\tArapahoe County: {Charles_Arapahoe:,} votes \n "
-# #     f"\tDenver County: {Charles_Denver:,} votes\n")
-
-# print(Diana_results)
-# # print(Charles_results)
-
